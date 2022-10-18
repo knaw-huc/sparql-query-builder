6ba560e7a544d1eee536651dd58db22d38e94e46
cskaandorp <casper@compunist.nl>
2022-10-18 08:24:47 +0200

    adapter

--- adapter/app.py
+++ adapter/app.py
@@ -3,23 +3,41 @@ import logging
 from flask import Flask, jsonify
 from flask_cors import CORS
 
+import sys
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://sys.stdout',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
+
 
 app = Flask(__name__)
-cors = CORS(app, resources={r'*': {
+cors = CORS(app, resources={r'/ga/*': {
     'origins': '*'
 }})
 
 API_URL = 'https://localhost:8080'
 
+@app.route('/', methods=['GET', 'OPTIONS', 'POST'])
+def agent():
+    response = jsonify({'data': 'agent'})
+    return response
 
 @app.route('/actuator/platform', methods=['GET', 'OPTIONS'])
 def platform():
     response = jsonify({'data': 'actuator'})
-    return response
-
-@app.route('/api/agent', methods=['GET', 'OPTIONS'])
-def agent():
-    response = jsonify({'data': 'agent'})
     return response
 
 @app.route('/api/agent/list', methods=['GET', 'OPTIONS'])
