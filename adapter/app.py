1e2d82dc7005972993fa0ffbb58d23ba6807f99b
cskaandorp <casper@compunist.nl>
2022-10-26 22:06:22 +0200

    how do I get all agents when using different TOML files

--- adapter/app.py
+++ adapter/app.py
@@ -4,7 +4,7 @@ import requests
 import sys
 
 from logging.config import dictConfig
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 dictConfig({
@@ -31,21 +31,63 @@ cors = CORS(app, resources={r'/ga/*': {
 
 API_URL = 'http://127.0.0.1:8080'
 
+# this dictionary stores all user agents
+user_agents = {}
+
 
 @app.route('/ga/getresources', methods=['GET'])
 def get_resources():
-    """This route gets all resources from the backend"""
+    """This route gets all resources from the backend and
+    returns the DB agents and stores the user agents"""
     try:
         response = requests.get(f'{API_URL}/api/agent/list')
         all_agents = json.loads(response.text)
-        resources = [
-            { 'id': agent['uuid'], 'name': agent['nickname'] } 
-            for agent in all_agents 
-            if agent['agentType'] == 'DB'
-        ]
+        print(all_agents)
+        # store all DB agents in this list
+        resources = []
+        for agent in all_agents:
+            print('AGENT', agent)
+            if agent['agentType'] == 'DB':
+                resources.append({
+                    'id': agent['uuid'],
+                    'name': agent['nickname']
+                })
+            elif agent['agentType'] == 'User':
+                agent_uuid = agent['uuid']
+                if agent_uuid not in user_agents.keys():
+                    user_agents[agent_uuid] = agent['nickname']
         return jsonify(resources)
     except:
         return []
+
+
+@app.route('/ga/sparql', methods=['POST'])
+def sparql():
+    """This route is for direct sparql queries"""
+    # breakdown request
+    req = json.loads(request.data.decode('UTF-8'))
+    # create a payload suitable for the GA backend
+    payload = {
+        'query': req['query'],
+        'queryType': 'USER_QUERY',
+        'selectedSources': [s['id'] for s in req['datasets']]
+    }
+    # for now, just pick the first user agent
+    user_agent_id = list(user_agents.keys())[0]
+    # send post request to GA backend
+    response = requests.post(
+        f'{API_URL}/api/agent/user/{user_agent_id}/query',
+        json=payload
+    )
+
+    print(response.text)
+
+
+    #print(request.data.['query'])
+    #print(request.data.decode('UTF-8')['datasets'])
+    return []
+
+
 
 @app.route('/ga/', methods=['GET', 'OPTIONS', 'POST'])
 def agent():
@@ -56,3 +98,4 @@ def agent():
 
 # http://127.0.0.1:5000/api/agent
 # http://127.0.0.1:5000/api/agent/list
+# /api/agent/user/USERAGENTID/query
