c314ae9bee8d5f155f29a3621af6f903a294ba74
Daan <daancjanssen@users.noreply.github.com>
2022-11-21 13:17:03 +0100

    QB basics setup

--- frontend/src/features/querybuilder/Builder.tsx
+++ frontend/src/features/querybuilder/Builder.tsx
@@ -1,11 +1,21 @@
-import React, {useEffect} from 'react';
+import React, {useState} from 'react';
 import {useAppSelector, useAppDispatch} from '../../app/hooks';
+import {AnimatePresence} from 'framer-motion';
 import {selectActiveQuery, selectSentQuery, setSentQuery} from './queryBuilderSlice';
 import Select from 'react-select';
 import styles from './QueryBuilder.module.scss';
 import * as queries from './helpers/queries';
 import {useSendSparqlQuery} from '../sparql/sparqlApi';
 import {setSelectedDatasets, selectedDatasets} from '../datasets/datasetsSlice';
+import Spinner from 'react-bootstrap/Spinner';
+import {FadeDiv} from '../animations/Animations';
+
+// TODO: Better typescript types
+
+interface SelectOptions {
+  label: string;
+  value: string;
+}
 
 const theme = (theme: any) => ({
   ...theme,
@@ -17,38 +27,112 @@ const theme = (theme: any) => ({
   }
 });
 
-const options = [
-  {value: 'pt', label: 'Painter'},
-  {value: 'wr', label: 'Writer'},
-  {value: 'ph', label: 'Philosopher'},
-  {value: 'sc', label: 'Scientist'},
-  {value: 'co', label: 'Colonist'},
-];
-
 export const Builder = () => {
   const dispatch = useAppDispatch();
   const currentDatasets = useAppSelector(selectedDatasets);
 
+  const [selectedEntities, setSelectedEntities] = useState([]);
+  const [selectedProperties, setSelectedProperties] = useState({});
+
+  // Do initial entity query
   const {data, isFetching, isError, error} = useSendSparqlQuery({
-    query: queries.initialQuery, 
+    query: queries.entityQuery, 
     datasets: currentDatasets
   });
 
-  const dataItems = data?.results.bindings;
+  // Do some filtering on results
+  const entities = data?.results.bindings;
+  // Must have a label
+  const entityOptions = entities && 
+    entities.filter( (item: any) => item.hasOwnProperty('l')).map( (item: any) => {
+      return {value: item.c.value, label: item.l.value}
+    })
+    // and no duplicates
+    .filter((v: SelectOptions, i: number, a: any[]) => a.findIndex((v2: SelectOptions) => (v2.label === v.label)) === i);
 
-  const options = dataItems && 
-    dataItems.map( (item: any) => { 
-      return {value: item.c.value, label: item.c.value} 
-    });
+  // Keep track of selected properties for each entity, passed down to PropertySelect
+  const setSelectedPropertiesObject = ( entity: string, newData: SelectOptions) => {
+    setSelectedProperties({...selectedProperties, [entity]: newData});
+  }
 
   return (
     <div>
       <h5 className={styles.header}>Build your query</h5>
+      <label id="entities-label" htmlFor="entities-input" className={styles.label}>
+        Select entities
+      </label>
       <Select 
+        aria-labelledby="entities-label"
+        inputId="entities-input"
         className={styles.select}
-        options={options} 
         placeholder="Give me every..."
-        theme={theme} />
+        theme={theme}
+        isMulti
+        noOptionsMessage={() => isFetching ? 
+          <Spinner
+            as="span"
+            animation="border"
+            size="sm"
+            role="status"
+            aria-hidden="true"/>  : 
+          'No properties found'
+        }
+        options={entityOptions}
+        onChange={(e: any) => setSelectedEntities(e)} />
+
+      <AnimatePresence>
+        {selectedEntities.map( (entity: SelectOptions, i: number) => 
+          <FadeDiv key={entity.value}>
+            <PropertySelect entity={entity} onChange={setSelectedPropertiesObject}/>
+          </FadeDiv>
+        )}
+      </AnimatePresence>
+
     </div>
   )
+}
+
+const PropertySelect = ({entity, onChange}: any) => {
+  const currentDatasets = useAppSelector(selectedDatasets);
+  const propertyQuery = queries.propertyQuery(entity.value);
+
+  const {data, isFetching, isError, error} = useSendSparqlQuery({
+    query: propertyQuery, 
+    datasets: currentDatasets
+  });
+
+  const properties = data?.results.bindings;
+
+  const propertyOptions = properties && 
+    properties.filter( (item: any) => item.hasOwnProperty('l')).map( (item: any) => {
+      return {value: item.pred.value, label: item.l.value}
+    })
+    .filter((v: SelectOptions, i: number, a: any[]) => a.findIndex((v2: SelectOptions) => (v2.label === v.label)) === i);
+
+  return (
+    <div>
+      <label id="property-label" htmlFor="property-input" className={styles.label}>
+        Properties for <span className={styles.labelEmphasis}>{entity.label}</span>
+      </label>
+      <Select 
+        aria-labelledby="property-label"
+        inputId="property-input"
+        className={styles.select}
+        options={propertyOptions} 
+        placeholder={`${entity.label} must have a...`}
+        isMulti
+        noOptionsMessage={() => isFetching ? 
+          <Spinner
+            as="span"
+            animation="border"
+            size="sm"
+            role="status"
+            aria-hidden="true"
+          /> : 
+          'No properties found'
+        }
+        onChange={(e: any) => onChange(entity.label, e)}
+        theme={theme} />
+    </div>
+  );
 }
--- frontend/src/features/querybuilder/helpers/queries.js
+++ frontend/src/features/querybuilder/helpers/queries.js
@@ -1,1 +1,26 @@
-export const initialQuery = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\nPREFIX owl: <http://www.w3.org/2002/07/owl#>\nPREFIX p: <http://www.wikidata.org/prop/>\nSELECT DISTINCT ?c\nWHERE { { ?c a rdfs:Class . }\n UNION { ?c a owl:Class . }\n FILTER ( !( ( BOUND(?c)\n && REGEX(str(?c), \"^(http://www.w3.org/2002/07/owl#|http://www.openlinksw.com/|nodeID://)\", 'i') ) ) ) }\nLIMIT 200";
+export const entityQuery = `
+  PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+  SELECT DISTINCT ?c ?l ?p
+  WHERE {
+    { ?s a ?c . }
+    OPTIONAL { ?c rdfs:label ?l }
+    OPTIONAL { ?c rdfs:subClassOf ?p }
+  }
+`;
+
+export const propertyQuery = (schema: string) => `
+  PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+  PREFIX owl: <http://www.w3.org/2002/07/owl#>
+  SELECT DISTINCT ?pred ?tpe ?dt ?ot ?l WHERE {
+    ?sub ?pred ?obj .
+    ?sub a <${schema}> .
+    BIND (
+      IF(isURI(?obj),
+        owl:ObjectProperty,
+        owl:DatatypeProperty) AS ?tpe
+      ) .
+    BIND ( DATATYPE(?obj) AS ?dt).
+    OPTIONAL {?obj a ?ot}.
+    OPTIONAL { ?pred rdfs:label ?l }
+  }
+`;
